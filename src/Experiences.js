09613bf125970029fb8d8a56fe3b58c83dc00d9f
Matheus Álefe <mabs2@cin.ufpe.br>
2024-11-27 08:23:23 -0300

    Atualização do código

--- src/Experiences.js
+++ src/Experiences.js
@@ -9,13 +9,13 @@ const Experiences = () => {
         {/* Tópico Gnomon */}
         <div className="experience-topic">
           <h2>Gnomon</h2>
-          <p>Descrição.</p>
+          <p>Projeto de extensão da UFPE onde foi realizado um estudo sobre patrimônio material e imaterial, seguido de curadoria e mapeamento de locais relacionados à igualdade étnico-racial em cidades pernambucanas. Utilizando o aplicativo Gnomon, inseriram-se pontos e rotas destacando legados históricos e narrativas culturais, com o objetivo de sensibilizar e promover o aprendizado de histórias esquecidas, contribuindo para uma sociedade mais justa e inclusiva em Pernambuco.</p>
         </div>
 
         {/* Tópico Inovaula */}
         <div className="experience-topic">
           <h2>Inovaula</h2>
-          <p>Descrição.</p>
+          <p>Desenvolvedor front-end, criando projetos utilizando tecnologias como JavaScript, HTML, CSS e Tailwind CSS, entre outras. Meu trabalho envolve a construção de interfaces web dinâmicas e responsivas, sempre buscando proporcionar uma experiência de usuário otimizada e visualmente agradável.</p>
         </div>
       </div>
     </div>
--- src/Projects.js
+++ src/Projects.js
@@ -7,15 +7,26 @@ const Projects = () => {
       <h1 className="projects-title">Projetos</h1>
       <div className="projects-container">
         {/* Primeiro projeto */}
-        <div className="project">
+        <a
+          href="https://github.com/MatheusHBSilva/Projeto-IP"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="project"
+        >
           <div className="project-icon kafka-icon"></div>
           <p className="project-name">Alice in Cocacafeland</p>
-        </div>
+        </a>
+        
         {/* Segundo projeto */}
-        <div className="project">
+        <a
+          href="https://github.com/inovaulaorg/inovaula-logger/tree/feature/frontend"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="project"
+        >
           <div className="project-icon rabbitmq-icon"></div>
           <p className="project-name">Inovaula Logger</p>
-        </div>
+        </a>
       </div>
     </div>
   );
